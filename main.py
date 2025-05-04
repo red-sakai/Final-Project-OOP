6004907a5e4ad6a7c78c1fb0a8b6795e80462fc3
red-sakai <jheredmiguelcrepublica@iskolarngbayan.pup.edu.ph>
2025-05-04 09:16:40 +0800

    feat: add class logic

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from abc import ABC, abstractmethod
 from enum import Enum
 
 class EmployeeStatus(Enum):
@@ -6,4 +7,24 @@ class EmployeeStatus(Enum):
     AWOL = "AWOL"
     DAY_OFF = "Day_off"
 
-    
+class Vehicle(ABC):
+    def __init__(self, gas_level: float, needs_maintenance: bool):
+        self.__gas_level = gas_level
+        self.__needs_maintenance = needs_maintenance
+
+    @property
+    def gas_level(self) -> float:
+        return self.__gas_level
+
+    @gas_level.setter
+    def gas_level(self, level: float):
+        self.__gas_level = level
+
+    @property
+    def needs_maintenance(self) -> bool:
+        return self.__needs_maintenance
+
+    @abstractmethod
+    def schedule_maintenance(self):
+        pass
+
