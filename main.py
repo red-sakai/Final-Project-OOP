5f7dc408938fc0bcfebf9dc1256529545151dee5
red-sakai <jheredmiguelcrepublica@iskolarngbayan.pup.edu.ph>
2025-05-06 18:50:10 +0800

    fix: fix certain classes

--- main.py
+++ main.py
@@ -181,27 +181,27 @@ class Database:
         pass
 
 # utilities class for different kinds of graphs
-class Utilities:
-    @staticmethod
-    def generate_salary_graph(employees: list):
-        pass
+# class Utilities:
+#     @staticmethod
+#     def generate_salary_graph(employees: list):
+#         pass
 
-    @staticmethod
-    def generate_employee_status_graph(employees: list):
-        status_counts = {}
-        for employee in employees:
-            status = employee.status.value
-            status_counts[status] = status_counts.get(status, 0) + 1
-        plt.bar(list(status_counts.keys()), list(status_counts.values()))
-        plt.title("Employee Status")
-        plt.show()
+#     @staticmethod
+#     def generate_employee_status_graph(employees: list):
+#         status_counts = {}
+#         for employee in employees:
+#             status = employee.status.value
+#             status_counts[status] = status_counts.get(status, 0) + 1
+#         plt.bar(list(status_counts.keys()), list(status_counts.values()))
+#         plt.title("Employee Status")
+#         plt.show()
 
-    @staticmethod
-    def generate_parcel_count_graph(parcels: list):
-        plt.bar(["Parcels", [len(parcels)]])
-        plt.title("Parcel Count")
-        plt.show()
+#     @staticmethod
+#     def generate_parcel_count_graph(parcels: list):
+#         plt.bar(["Parcels", [len(parcels)]])
+#         plt.title("Parcel Count")
+#         plt.show()
 
-    @staticmethod
-    def generate_vehicle_usage_stats(vehicles: list):
-        pass
+#     @staticmethod
+#     def generate_vehicle_usage_stats(vehicles: list):
+#         pass
